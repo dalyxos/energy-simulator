2fbeb86148b4694f120946cbb1703eeb32e4c480
Mohamed-Ali Daghfous <med.ali.daghfous@gmail.com>
2025-02-22 02:25:22 +0100

    Add API version endpoint and update frontend to display it

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Welcome to the Energy Simulator!"
+
+@app.route('/api/version')
+def version():
+    return {"version": "0.1.0"}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#cee',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+function BasicGrid() {
+
+  const [apiVersion, setApiVersion] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('/api/version')
+      .then(response => response.json())
+      .then(data => setApiVersion(data.version))
+      .catch(error => console.error('Error fetching API version:', error));
+  }, []);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid size={12}>
+          <Item sx={{ backgroundColor: '#033', color: '#FFF' }}>{apiVersion ? `API Version: ${apiVersion}` : 'Loading...'}</Item>
+        </Grid>
+        <Grid size={3}>
+          <Item>Load Info</Item>
+        </Grid>
+        <Grid size={3}>
+          <Item>Charging Stations</Item>
+        </Grid>
+        <Grid size={3}>
+          <Item>Inverters</Item>
+        </Grid>
+        <Grid size={3}>
+          <Item>Heat Pump</Item>
+        </Grid>
+        <Grid size={12}>
+          <Item>Settings / Config</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BasicGrid />
   );
 }
 
